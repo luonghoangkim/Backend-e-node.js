22e97f8eca292c1bddd2a7b9a403ed53a12da006
luonghoangkim <akashi192002@gmail.com>
2023-10-06 14:49:33 +0700

    Product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const UserRouter = require('./UserRouter')
+const ProductRouter = require('./ProductRouter')
 
 const routes = (app) => {
     app.use('/api/user', UserRouter)
-    // app.use('/api/product', ProductRouter)
+    app.use('/api/product', ProductRouter)
     // app.use('/api/order', OrderRouter)
     // app.use('/api/payment', PaymentRouter)
 }
